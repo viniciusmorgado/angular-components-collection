d65c319aedfa4a2cf84b34ae77cbe6967c93c236
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2024-08-23 20:46:39 -0300

    add order summary components

---
+++ components/order-summary/angular-css/src/app/order/order-sumarry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-sumarry',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-sumarry.component.html',
+  styleUrl: './order-sumarry.component.css',
+})
+export class OrderSumarryComponent {}
